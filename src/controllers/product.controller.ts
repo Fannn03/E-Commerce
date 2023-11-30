f868834fb7ee04229a2b8f188ceaf6aaa30540df
Fannn03 <fann.fann771@gmail.com>
2023-11-30 14:24:44 +0700

    feat: add pagination product using params query

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,28 @@
 import { Request, Response } from "express";
 import createProductService, { CreateProductError } from '../services/products/create.service';
+import findAllProductService from "../services/products/findall.service";
+
+export const findAllProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await findAllProductService(req.query);
+
+    if(!products && req.query) return res.status(404).json({
+      code: 404,
+      result: 'not found',
+      message: 'record not found',
+      data: products
+    })
+
+    return res.json({
+      code: 200,
+      result: 'success',
+      message: 'success get record data',
+      data: products
+    })
+  } catch (err) {
+
+  }
+}
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -3,11 +3,12 @@ import multer from 'multer';
 import { multerStorage } from '../config/multer';
 import authMiddleware from '../middleware/auth.middleware';
 import createProductMiddleware from '../middleware/products/create.product.middleware';
-import { createProduct } from '../controllers/product.controller';
+import { createProduct, findAllProducts } from '../controllers/product.controller';
 
 const router: Router = express.Router();
 const file = multer({ storage: multerStorage() })
 
+router.get('/', findAllProducts);
 router.post('/create', authMiddleware, file.array('photo'), createProductMiddleware, createProduct);
 
 export default router;
