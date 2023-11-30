f3283991c25ae1acf92be369539891ec86d3860c
Fannn03 <fann.fann771@gmail.com>
2023-11-30 17:35:43 +0700

    feat: delete product

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -3,15 +3,16 @@ import multer from 'multer';
 import { multerStorage } from '../config/multer';
 import authMiddleware from '../middleware/auth.middleware';
 import createProductMiddleware from '../middleware/products/create.product.middleware';
-import { createProduct, detailProduct, findAllProducts, updateProduct } from '../controllers/product.controller';
 import updateProductMiddleware from '../middleware/products/update-product.middleware';
+import { createProduct, deleteProduct, detailProduct, findAllProducts, updateProduct } from '../controllers/product.controller';
 
 const router: Router = express.Router();
-const file = multer({ storage: multerStorage() })
+const file = multer({ storage: multerStorage() });
 
 router.get('/', findAllProducts);
 router.post('/create', authMiddleware, file.array('photo'), createProductMiddleware, createProduct);
 router.get('/:slug', detailProduct);
 router.put('/:id', authMiddleware, updateProductMiddleware, updateProduct);
+router.delete('/:id', authMiddleware, deleteProduct);
 
 export default router;
