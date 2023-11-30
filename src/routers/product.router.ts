e5a5f8eb3059989017cb8ae1889cf9fd6f15c1a2
Fannn03 <fann.fann771@gmail.com>
2023-11-30 13:21:14 +0700

    feat: add create product service

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -3,10 +3,11 @@ import multer from 'multer';
 import { multerStorage } from '../config/multer';
 import authMiddleware from '../middleware/auth.middleware';
 import createProductMiddleware from '../middleware/products/create.product.middleware';
+import { createProduct } from '../controllers/product.controller';
 
 const router: Router = express.Router();
 const file = multer({ storage: multerStorage() })
 
-router.post('/create', authMiddleware, file.array('photo'), createProductMiddleware);
+router.post('/create', authMiddleware, file.array('photo'), createProductMiddleware, createProduct);
 
 export default router;
