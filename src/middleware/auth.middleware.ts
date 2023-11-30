c8ecf7537f9bab2da75ce7717e3f093d59895db6
Fannn03 <fann.fann771@gmail.com>
2023-11-30 10:07:18 +0700

    fix: missing return

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -18,7 +18,7 @@ declare global {
 export default async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1]
 
-  if(!token) res.status(401).json({
+  if(!token) return res.status(401).json({
     code: 401,
     result: 'unauthorized',
     message: 'token does not provided'
