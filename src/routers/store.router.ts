6aa82f872b7548f12d8617b6dd821b6baafa9904
Fannn03 <fann.fann771@gmail.com>
2023-11-30 10:16:42 +0700

    feat: create store middleware using multer

--- src/routers/store.router.ts
+++ src/routers/store.router.ts
@@ -1,8 +1,12 @@
 import express, { Router } from 'express';
 import authMiddleware from '../middleware/auth.middleware';
+import createStoreMiddleware from '../middleware/stores/create.store.middleware';
+import multer from 'multer';
+import { multerStorage } from '../config/multer';
 
 const router: Router = express.Router();
+const file = multer({ storage: multerStorage() })
 
-router.post('/create', authMiddleware);
+router.post('/create', authMiddleware, file.single('photo'), createStoreMiddleware);
 
 export default router;
