68b208ccaa4ea9a925cae638c8a6c768a203a17b
Fannn03 <fann.fann771@gmail.com>
2023-11-30 09:27:39 +0700

    feat: create user login

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import userRegisterService, { UserRegisterError } from '../services/users/register.service';
+import userLoginService, { UserLoginError } from "../services/users/login.service";
 
 export const userRegister = async (req: Request, res: Response) => {
   try {
@@ -19,7 +20,39 @@ export const userRegister = async (req: Request, res: Response) => {
         message: err.message
       })
     }
+    return res.status(500).json({
+      code: 500,
+      result: 'internal server error',
+      message: err.message
+    })
+  }
+}
 
+export const userLogin = async (req: Request, res: Response) => {
+  try {
+    const user = await userLoginService(req.body);
+
+    if(!user) return res.status(404).json({
+      code: 404,
+      result: 'not found',
+      message: 'record not found',
+      data: user
+    })
+
+    return res.json({
+      code: 200,
+      result: "ok",
+      message: 'login success',
+      data: user
+    })
+  } catch (err: any) {
+    if(err instanceof UserLoginError) {
+      return res.status(err.code).json({
+        code: err.code,
+        result: err.result,
+        message: err.message
+      })
+    }
     return res.status(500).json({
       code: 500,
       result: 'internal server error',
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express';
 import userRegisterMiddleware from '../middleware/users/user.register.middleware';
 import userLoginMiddleware from '../middleware/users/user.login.middleware';
-import { userRegister } from '../controllers/user.controller';
+import { userLogin, userRegister } from '../controllers/user.controller';
 
 const router: Router = express.Router();
 
 router.post('/create', userRegisterMiddleware, userRegister);
-router.post('/login', userLoginMiddleware);
+router.post('/login', userLoginMiddleware, userLogin);
 
 export default router;
